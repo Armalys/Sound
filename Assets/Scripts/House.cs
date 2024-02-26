2e5bf7d1323ab3b170f1d4023c59488617b9e9e0
Armalys <mashnert@mail.ru>
2024-02-26 18:15:39 +0100

    правка на TryGetComponent

--- Assets/Scripts/House.cs
+++ Assets/Scripts/House.cs
@@ -6,7 +6,7 @@ public class House : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.GetComponent<Thief>())
+        if (other.TryGetComponent(out Thief thief ))
         {
             _alarm.TurnOn();
         }
@@ -14,7 +14,7 @@ public class House : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.GetComponent<Thief>())
+        if (other.TryGetComponent(out Thief thief ))
         {
             _alarm.TurnOff();
         }
